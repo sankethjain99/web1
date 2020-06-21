710ca0ce67fb0846761e8e983a4c26014ed05f16
sanketh <jsanketh1799@gmail.com>
2020-06-21 10:21:58 +0530

    npm scripts part1

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+
+
+$(document).ready(function(){
+    $("#newModal").modal("hide");
+    $("#button1").click(function(){
+      $("#newModal").modal("show");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#mod").modal("hide");
+    $("#but").click(function(){
+      $("#mod").modal("show");
+    });
+  });
